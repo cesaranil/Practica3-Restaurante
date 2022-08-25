8752177828fb7d65294bf55a640c8802028187f6
cesaranil <8182cesar17@gmail.com>
2022-08-24 19:52:01 -0500

    Agregando los efectos al header

--- js/efectos.js
+++ js/efectos.js
@@ -10,4 +10,17 @@ $(document).ready(function(){
         }, 2000 + (index * 500))
     })
 
+    //Efecto del Header
+    if($(window).width() > 800){
+        $('header .texto').css({
+            opacity:0,
+            marginTop:0
+        })
+
+        $('header .texto').animate({
+            opacity:1,
+            marginTop: '-52px'
+        }, 1500)
+    }
+
 })
