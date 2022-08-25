d2b5da78c429e5a023a39fecdd676db0f0988adf
cesaranil <8182cesar17@gmail.com>
2022-08-24 20:06:19 -0500

    Agregando el Scroll del menu

--- js/efectos.js
+++ js/efectos.js
@@ -23,4 +23,37 @@ $(document).ready(function(){
         }, 1500)
     }
 
+    //Scroll Menu
+    var acercaDe = $('#acerca-de').offset().top,
+        menu = $('#platillo').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top
+
+    $('#btn-acerca-de').on('click', function(e){
+        e.preventDefault()
+        $('html, body').animate({
+            scrollTop: acercaDe -100
+        }, 500)
+    })
+
+    $('#btn-menu').on('click', function(e){
+        e.preventDefault()
+        $('html, body').animate({
+            scrollTop: menu
+        }, 500)
+    })
+
+    $('#btn-galeria').on('click', function(e){
+        e.preventDefault()
+        $('html, body').animate({
+            scrollTop: galeria
+        }, 500)
+    })
+
+    $('#btn-ubicacion').on('click', function(e){
+        e.preventDefault()
+        $('html, body').animate({
+            scrollTop: ubicacion
+        }, 500)
+    })
 })
